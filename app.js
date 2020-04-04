39fb50c4e5935f81d9fb88b7bd0cbc95ff47bcc7
Deepanshu <kathuriadeepanshu421@gmail.com>
2020-04-04 18:11:46 +0530

    Added the secrets file

---
+++ app.js
@@ -0,0 +1,69 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+let mongooseFieldEncryption = require('mongoose-field-encryption').fieldEncryption
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useUnifiedTopology: true, useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String
+});
+
+userSchema.plugin(mongooseFieldEncryption, {secret: process.env.SECRET, fields: ["password"]});
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+	res.render("home");
+});
+
+app.get('/login', (req, res) => {
+	res.render("login");
+});
+
+app.get('/register', (req, res) => {
+	res.render("register");
+});
+
+app.post('/register', (req, res) => {
+	const newUser = new User({
+		email: req.body.username,
+		password: req.body.password
+	});
+
+	newUser.save((err) => {
+		res.render('secrets');
+	});
+});
+
+app.post('/login', (req, res) => {
+	const email= req.body.username;
+	const password= req.body.password;
+
+	User.findOne({email: email}, (err, user) => {
+		if(!err && user && user.password === password)
+		{
+			res.render('secrets');
+		}
+		else
+		{
+			console.log(err);
+		}
+	});
+})
+
+app.listen(3000, () => {
+	console.log('Server Started');
+})
